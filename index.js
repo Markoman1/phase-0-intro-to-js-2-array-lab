17ed7be3952e9e8814c8fe3d8244edca3753d236
Mark <mgalex12@icloud.com>
2022-02-17 14:45:02 -0500

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+// const copyOfCats = cats.slice()
+// console.log(copyOfCats)
+
+// //const destructivelyAppendCat = ["Milo", "Otis", "Garfield"]
+// destructivelyAppendCat.push("Ralph")
+// console.log(destructivelyAppendCat)
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+ function appendCat(name) {
+ const copyOfAppendCat = [...cats, name];
+ return copyOfAppendCat
+ }
+
+ function prependCat(name) {
+ const copyOfPrependCat = [name, ...cats];
+ return copyOfPrependCat
+ }
+
+ function removeLastCat(name) {
+     const copyOfRemoveLastCat = cats.slice(0, cats.length - 1)
+     return copyOfRemoveLastCat
+ }
+
+ function removeFirstCat(name) {
+     const copyOfRemoveFirstCat = cats.slice(1);
+     return copyOfRemoveFirstCat
+ }
